990285679fd82c2e1fddbcc9a7cacd6d675b74dd
AbdurRob <abdurrobr34l@gmail.com>
2025-10-19 17:33:01 +0600

    Qzone component completed

--- src/Components/Homelayout/RightAside.jsx
+++ src/Components/Homelayout/RightAside.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SocialLogins from './SocialLogins';
 import FindUs from '../FindUs';
+import Qzone from '../Qzone';
 
 const RightAside = () => {
   return (
     <div>
       <SocialLogins></SocialLogins>
       <FindUs></FindUs>
+      <Qzone></Qzone>
     </div>
   );
 };
--- src/Components/Qzone.jsx
+++ src/Components/Qzone.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import swimmingImage from "../assets/swimming.png";
+import classImage from "../assets/class.png";
+import playImage from "../assets/playground.png";
 const Qzone = () => {
   return (
-    <div>
-      
+    <div className="mt-5 p-3 bg-base-200">
+      <h2 className="font-bold mb-5">Q-Zone</h2>
+      <div className="space-y-5">
+        <img src={swimmingImage} alt="It is an little girl swimming image" />
+        <img src={classImage} alt="It is can class room image" />
+        <img src={playImage} alt="It is childrens playing image" />
+      </div>
     </div>
   );
 };
