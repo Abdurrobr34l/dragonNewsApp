defca7ec62418ead63559217adb8a7fd3ae8f8d2
AbdurRob <abdurrobr34l@gmail.com>
2025-10-19 16:07:53 +0600

    Created and showing news card dyanmically

--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,13 +1,79 @@
-import React from 'react';
+import React from "react";
+import { FaRegBookmark } from "react-icons/fa";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { Link } from "react-router";
 
-const NewsCard = ({newsByCategory}) => {
+const NewsCard = ({ newsByCategory }) => {
   return (
-    <div>
-      {
-        console.log("from NewsCard", newsByCategory)
-      }
-      <div className='mt-5'>{newsByCategory.length}</div>
-    </div>
+    <>
+      <div>
+        {newsByCategory.map((news) => (
+          <div className="mt-10 first:mt-0 border border-base-300 rounded-lg">
+            {/* Author & Share */}
+            <div className="flex items-center justify-between p-7 py-4 bg-base-300 rounded-t-lg">
+              {/*//? Author */}
+              <div className="flex items-center gap-3">
+                {/*//? Author Image */}
+                <div>
+                  <img
+                    src={news.author.img}
+                    alt="It is author image of this blog"
+                    className="size-20 rounded-full"
+                  />
+                </div>
+
+                {/*//? Author Name */}
+                <div>
+                  <p className="text-xl font-bold text-primary">
+                    {news.author.name}
+                  </p>
+                  <p className="text-primary">{news.author.published_date}</p>
+                </div>
+              </div>
+
+              {/*//? Share */}
+              <div className="flex gap-3">
+                <button type="button" className="cursor-pointer">
+                  <FaRegBookmark className="text-2xl text-primary" />
+                </button>
+
+                <button type="button" className="cursor-pointer">
+                  <IoShareSocialOutline className="text-2xl text-primary" />
+                </button>
+              </div>
+            </div>
+
+            {/* Contents */}
+            <div className="p-7 pt-5">
+              {/* Title */}
+              <h2 className="my-5 text-2xl font-bold text-primary">
+                {news.title}
+              </h2>
+
+              {/* Image */}
+              <img
+                src={news.image_url}
+                alt="It is news image"
+                className="w-full rounded-lg"
+              />
+
+              {/* Description & Button */}
+              <div className="mt-12">
+                <p className="text-lg">{news.details}</p>
+                <Link className="btn btn-ghost btn-primary mt-2 p-0 text-md font-bold border-none hover:text-accent hover:bg-transparent hover:border-none hover:shadow-none">
+                  Read More
+                </Link>
+              </div>
+
+              <hr className="my-5 text-base-300" />
+
+              {/* Rating & View */}
+              <div></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -6,19 +6,21 @@ import NewsCard from "../Components/NewsCard";
 const CategoryNews = () => {
   const { id } = useParams();
   // const newsData = useLoaderData(); //* All 58 news data
-    const { newsData } = useOutletContext()
+  const { newsData } = useOutletContext();
   const [newsByCategory, setNewsByCategory] = useState([]);
 
   useEffect(() => {
     if (id == "0") {
       setNewsByCategory(newsData); //* show all 58 news data
       return;
-    } else if (id == "1") { //* This one to find Breaking news data only
+    } else if (id == "1") {
+      //* This one to find Breaking news data only
       const filterBreakingNews = newsData.filter(
         (breakingNews) => breakingNews.others.is_today_pick == true
       );
       setNewsByCategory(filterBreakingNews);
-    } else { //* Show news by id
+    } else {
+      //* Show news by id
       const filterNewsById = newsData.filter(
         (newsById) => newsById.category_id == id
       );
@@ -29,8 +31,11 @@ const CategoryNews = () => {
 
   return (
     <div>
-      <h2 className="font-semibold">Dragon News Home</h2>
-      <NewsCard newsByCategory={newsByCategory}></NewsCard>
+      <h2 className="mb-5 font-semibold">Dragon News Home</h2>
+
+      <div className="col-span-1">
+        <NewsCard newsByCategory={newsByCategory}></NewsCard>
+      </div>
     </div>
   );
 };
