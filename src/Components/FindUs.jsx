f9f8218d689d27c2999aef4947f4247287be5425
AbdurRob <abdurrobr34l@gmail.com>
2025-10-19 17:24:18 +0600

    Find us component done

--- src/Components/FindUs.jsx
+++ src/Components/FindUs.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const FindUs = () => {
   return (
     <div>
-      FindUs
+      <h2 className="mt-10 mb-5 font-semibold">Find Us On</h2>
+
+      <div className="join join-vertical w-full">
+        <button className="btn findus-button rounded-t-lg">
+          <span className="p-2 bg-base-300 text-[#1877F2] rounded-full">
+          <FaFacebookF/>
+          </span>
+
+          Facebook
+        </button>
+
+        <button className="btn findus-button">
+          <span className="p-2 bg-base-300 text-[#1DA1F2] rounded-full">
+          <FaTwitter />
+          </span>
+
+          Twitter
+        </button>
+
+        <button className="btn findus-button rounded-b-lg">
+          <span className="p-2 bg-base-300 text-[#E1306C] rounded-full">
+          <FaInstagram />
+          </span>
+
+          Instagram
+        </button>
+      </div>
     </div>
   );
 };
--- src/Components/Homelayout/RightAside.jsx
+++ src/Components/Homelayout/RightAside.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SocialLogins from './SocialLogins';
+import FindUs from '../FindUs';
 
 const RightAside = () => {
   return (
     <div>
       <SocialLogins></SocialLogins>
+      <FindUs></FindUs>
     </div>
   );
 };
