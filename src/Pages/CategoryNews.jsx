a91002d365728f6590ce43fb572f263a5875fd41
AbdurRob <abdurrobr34l@gmail.com>
2025-10-19 11:52:24 +0600

    Showing news by id

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-  const {id} = useParams()
-  return (
-    <div>
-      CategoryNews - {id}
-    </div>
-  );
+  const { id } = useParams();
+  const newsData = useLoaderData(); //* All 58 news data
+  const [newsByCategory, setNewsByCategory] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setNewsByCategory(newsData); //* show all 58 news data
+      return;
+    } else if (id == "1") {  //* This one to find Breaking news data only
+      const filterBreakingNews = newsData.filter(
+        (breakingNews) => breakingNews.others.is_today_pick == true
+      );
+      setNewsByCategory(filterBreakingNews);
+    } else { //* Show news by id
+      const filterNewsById = newsData.filter(
+        (newsById) => newsById.category_id == id
+      );
+
+      setNewsByCategory(filterNewsById);
+    }
+  }, [id, newsData]);
+
+  return <div>CategoryNews - {newsByCategory.length}</div>;
 };
 
 export default CategoryNews;
